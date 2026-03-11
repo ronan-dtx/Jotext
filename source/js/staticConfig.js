4ad8ff5db0ec9124a404bf51f00d05a1de024d29
Ronan <267252292+ronan-dtx@users.noreply.github.com>
2026-03-11 14:52:31 +0800

    现已登陆Github

---
+++ source/js/staticConfig.js
@@ -0,0 +1,12 @@
+const PROJECT_NAME = "Jotext";
+const LATEST_DATA_KEY = "local_storage_record_data_v6.0";
+const VALID_COLOR_MODE = {AUTO:"跟随系统",LIGHT:"浅色",DARK:"深色"};
+const THEME_COLOR_OPTION = [
+								{name:"蓝色",value:"blueTheme",color: "rgb(0, 153, 255)"},
+								{name:"橙色",value:"orangeTheme",color: "rgb(226, 85, 7)"},
+								{name:"紫色",value:"purpleTheme",color: "rgb(128, 70, 217)"},
+								{name:"黄色",value:"yellowTheme",color: "rgb(224, 172, 0)"},
+								{name:"绿色",value:"greenTheme",color: "rgb(0, 162, 64)"}
+							];
+const EARLIEST_YEAR_OPTION = 2020;
+const LATEST_YEAR_OPTION_AFTER_NOW = 10;
